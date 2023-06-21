3337fd828130699fd0301cf804ed158995d48416
BSA <johndoe@example.com>
2023-06-22 01:12:49 +0300

    add orm & post model

--- app/config.py
+++ app/config.py
@@ -1,2 +1,3 @@
 class Configuration(object):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:123@localhost/flask_om'
---
+++ app/models.py
@@ -0,0 +1,27 @@
+from app import db
+from datetime import datetime
+import re
+
+
+def slugify(s: str) -> str:
+    pattern = r'[^\w+]'
+    return re.sub(pattern, '-', s)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140))
+    slug = db.Column(db.String(140), unique=True)
+    body = db.Column(db.Text)
+    created = db.Column(db.DateTime, default=datetime.now())
+
+    def __init__(self, *args, **kwargs):
+        super(Post, self).__init__(*args, **kwargs)
+        self.generate_slug()
+
+    def generate_slug(self):
+        if self.title:
+            self.slug = slugify(self.title)
+
+    def __repr__(self):
+        return f'<Post id: {self.id}, title: {self.title}>'
